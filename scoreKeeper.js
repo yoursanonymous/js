ab7f72b6c777b0b39ef238674203039f5998dfce
yoursanonymous <vinayaks0111@gmail.com>
2024-09-05 02:09:42 +0530

    b

--- scoreKeeper.js
+++ scoreKeeper.js
@@ -1,6 +1,8 @@
 const p1Btn = document.querySelector('#p1Button');
 const p2Btn = document.querySelector('#p2Button');
 const reset= document.querySelector('#reset');
+const playTo=document.querySelector('#playto');
+
 
 const p2Display = document.querySelector('#p2Display');
 const p1Display = document.querySelector('#p1Display');
@@ -15,6 +17,8 @@ p1Btn.addEventListener('click', function (e) {
     p1Score++;
     if(p1Score===winningScore){
         gameOver=true;
+        p1Display.classList.add('winner');
+        p2Display.classList.add('loser');
     }
     p1Display.innerText = p1Score;
     }
@@ -25,12 +29,25 @@ p2Btn.addEventListener('click', function (e) {
     p2Score++;
     if(p2Score===winningScore){
         gameOver=true;
+        p2Display.classList.add('winner');
+        p1Display.classList.add('loser');
     }
     p2Display.innerText = p2Score;
    }
 })
 
-reset.addEventListener('click',function(e){
+playTo.addEventListener('click',function(){
+    playTo=parseInt(this.value);
+    reset();
+})
+
+reset.addEventListener('click',reset())
+function reset(){
+    gameOver=false
     p2Score=0;
     p1Score=0;
-})
+    p1Display.innerText=0;
+    p2Display.innerText=0;
+    p2Display.classList.remove('winner','loser');
+    p1Display.classList.remove('winner','loser');
+}
