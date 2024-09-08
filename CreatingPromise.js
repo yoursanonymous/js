593670ff201f98d7d2781dfb9f82bbfa8fd224e9
yoursanonymous <vinayaks0111@gmail.com>
2024-09-08 14:27:24 +0530

    h

--- CreatingPromise.js
+++ CreatingPromise.js
@@ -6,16 +6,35 @@ const fakeRequest = (url) => {
             if(rand<0.7){
                 resolve('YOUR FAKE DATA IS HERE');
             }
-            reject('REQUEST ERROR');
+            reject('CONNECTION TIMEOUT');
         }, 1000)
     })
 }
 
-fakeRequest('/dogs/1')
-    .then((data)=>{
-        console.log("DONE IW")
-        console.log('data is: ', data)
-    })
-    .catch((err)=>{
-        console.log('oh no!', err);
-    })
+async function makeTwoRequest(){
+    try{
+        let data1= await  fakeRequest('/page2');
+        let data2= await  fakeRequest('/page2');
+        console.log(data1)
+        console.log(data2)
+        console.log("HELOO!!!!")
+    }
+    catch(e){
+        console.log("caught an error", "and the error is :",e);
+    }
+}
+makeTwoRequest()
+// try{
+//     sadgg.log('asags')
+// }catch(e){
+//     console.log("hhhhhhhhhhhhh",e)
+// }
+
+// fakeRequest('/dogs/1')
+//     .then((data)=>{
+//         console.log("DONE IW")
+//         console.log('data is: ', data)
+//     })
+//     .catch((err)=>{
+//         console.log('oh no!', err);
+//     })
