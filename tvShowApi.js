641217c4d9ec579ddfcd043fac19237e7dca101b
yoursanonymous <vinayaks0111@gmail.com>
2024-10-03 23:59:54 +0530

    h

--- tvShowApi.js
+++ tvShowApi.js
@@ -1,7 +1,21 @@
-const form= document.querySelector('#searchForm')
-form.addEventListener('submit',function(e){
+// const form= document.querySelector('#searchForm')
+// form.addEventListener('submit',function(e){
+//     e.preventDefault();
+//     // const user=form.elements.query.value;
+//     console.log(form.elements.query.value);
+//     axios.get('https://api.tvmaze.com/search/shows?q=girls');
+// })
+
+const form = document.querySelector('#searchForm');
+form.addEventListener('submit', function(e) {
     e.preventDefault();
-    // const user=form.elements.query.value;
-    console.log(form.elements.query.value);
-    axios.get('https://api.tvmaze.com/search/shows?q=girls');
-})
+    const userQuery = form.elements.query.value;
+    console.log(userQuery);
+    axios.get(`https://api.tvmaze.com/search/shows?q=${userQuery}`)
+        .then(response => {
+            console.log(response.data);
+        })
+        .catch(error => {
+            console.error('Error fetching data:', error);
+        });
+});
